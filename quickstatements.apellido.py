d207281e6f938210f3f9c656b6000aa68b350327
emijrp <emijrp@gmail.com>
2017-02-06 15:57:43 +0100

    male given name, female given name, surname

--- quickstatements.apellido.py
+++ quickstatements.apellido.py
@@ -21,84 +21,7 @@ import pwb
 import pywikibot
 from quickstatements import *
 
-def main():
-    translations = {
-        #'an': '', #no esta claro si es apellido o apelliu?
-        'ar': 'اسم العائلة', 
-        'ast': 'apellíu', 
-        'az': 'Soyad', 
-        'bar': 'Schreibnam', 
-        'be': 'прозвішча', 
-        'bg': 'презиме', 
-        'bn': 'পারিবারিক নাম', 
-        'bs': 'prezime', 
-        'ca': 'cognom', 
-        'cs': 'příjmení', 
-        'cy': 'cyfenw', 
-        'da': 'efternavn', 
-        'de': 'Familienname', 
-        'de-at': 'Familienname', 
-        'de-ch': 'Familienname', 
-        'el': 'επώνυμο', 
-        'en': 'family name', 
-        'es': 'apellido', 
-        'et': 'perekonnanimi', 
-        'eu': 'abizen', 
-        'fa': 'نام خانوادگی', 
-        'fi': 'sukunimi', 
-        'fo': 'ættarnavn',
-        'fr': 'nom de famille', 
-        'gl': 'apelido', 
-        'gsw': 'Familiename', 
-        'gu': 'અટક', 
-        'he': 'שם משפחה', 
-        'hr': 'prezime', 
-        'hu': 'vezetéknév', 
-        'hy': 'ազգանուն', 
-        'id': 'nama asli', 
-        'is': 'eftirnafn',
-        'it': 'cognome', 
-        'ja': '姓', 
-        'ka': 'გვარი', 
-        'ko': '성씨', 
-        'lb': 'Familljennumm', 
-        'lt': 'pavardė', 
-        'lv': 'uzvārds', 
-        'min': 'namo asli', 
-        'mk': 'презиме', 
-        'nb': 'etternavn', 
-        'nds': 'Familiennaam', 
-        'nl': 'achternaam', 
-        'nn': 'etternamn', 
-        'pl': 'nazwisko', 
-        'pt': 'sobrenome', 
-        'pt-br': 'nome de família', 
-        'ro': 'nume de familie', 
-        'ru': 'фамилия', 
-        'sh': 'prezime', 
-        'sk': 'priezvisko', 
-        'sl': 'priimek', 
-        'sq': 'mbiemri', 
-        'sr': 'презиме', 
-        'sv': 'efternamn', 
-        'tl': 'apelyido', 
-        'tr': 'soyadı', 
-        'uk': 'прізвище', 
-        'zh': '姓氏', 
-        'zh-cn': '姓氏', 
-        'zh-hans': '姓氏', 
-        'zh-hant': '姓氏', 
-        'zh-hk': '姓氏', 
-        'zh-mo': '姓氏', 
-        'zh-my': '姓氏', 
-        'zh-sg': '姓氏', 
-        'zh-tw': '姓氏',  
-        'zu': 'isibongo', 
-    }
-    site = pywikibot.Site('wikidata', 'wikidata')
-    repo = site.data_repository()
-    
-    """
+"""
     #filter by language
     SELECT ?item
     WHERE {
@@ -113,35 +36,262 @@ def main():
         ?item wdt:P31 wd:Q101352 .
         FILTER NOT EXISTS { ?item wdt:P31 wd:Q4167410 } . 
     }
-    """
-    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=SELECT%20%3Fitem%0AWHERE%20%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3AQ101352%20.%0A%20%20%20%20FILTER%20NOT%20EXISTS%20%7B%20%3Fitem%20wdt%3AP31%20wd%3AQ4167410%20%7D%20.%20%0A%7D'
-    url = '%s&format=json' % (url)
-    sparql = getURL(url=url)
-    json1 = loadSPARQL(sparql=sparql)
+"""
+
+def main():
+    translations = {
+        'Q11879590': { #female given name
+            'af': 'vroulike voornaam',
+            'ar': 'أسم مؤنث معطى',
+            'ast': 'nome femenín',
+            'bar': 'Weiwanam',
+            'be': 'жаночае асабістае імя',
+            'bn': 'প্রদত্ত মহিলা নাম',
+            'br': 'anv merc’hed',
+            'bs': 'žensko ime',
+            'ca': 'prenom femení',
+            'ce': 'зудчун шен цӀе',
+            'cs': 'ženské křestní jméno',
+            'cy': 'enw personol benywaidd',
+            'da': 'pigenavn',
+            'de': 'weiblicher Vorname',
+            'de-at': 'weiblicher Vorname',
+            'de-ch': 'weiblicher Vorname',
+            'el': 'γυναικείο όνομα',
+            'en': 'female given name',
+            'en-ca': 'female given name',
+            'en-gb': 'female given name',
+            'eo': 'virina persona nomo',
+            'es': 'nombre femenino',
+            'et': 'naisenimi',
+            'fa': 'نام‌های زنانه',
+            'fi': 'naisen etunimi',
+            'fr': 'prénom féminin',
+            'he': 'שם פרטי של אישה',
+            'hr': 'žensko ime',
+            'hsb': 'žonjace předmjeno',
+            'hu': 'női keresztnév',
+            'hy': 'իգական անձնանուն',
+            'id': 'nama perempuan feminin',
+            'it': 'prenome femminile',
+            'ja': '女性の名前',
+            'ko': '여성의 이름',
+            'la': 'praenomen femininum',
+            'lb': 'weibleche Virnumm',
+            'lt': 'moteriškas vardas',
+            'lv': 'sieviešu personvārds',
+            'mk': 'женско лично име',
+            'nb': 'kvinnenavn',
+            'ne': 'स्त्रीलिङ्गी नाम',
+            'nl': 'vrouwelijke voornaam',
+            'nn': 'kvinnenamn',
+            'pl': 'imię żeńskie',
+            'pt': 'nome próprio feminino',
+            'pt-br': 'nome próprio feminino',
+            'ro': 'prenume feminin',
+            'ru': 'женское личное имя',
+            'sr': 'женско лично име',
+            'sr-ec': 'женско лично име',
+            'scn': 'nomu di battìu fimmininu',
+            'sco': 'female gien name',
+            'sk': 'ženské krstné meno',
+            'sl': 'žensko osebno ime',
+            'sr-el': 'žensko lično ime',
+            'sv': 'kvinnonamn',
+            'tr': 'kadın ismidir',
+            'uk': 'жіноче особове ім’я',
+            'yue': '女性人名',
+            'zh': '女性人名',
+            'zh-cn': '女性人名 ',
+            'zh-hans': '女性人名',
+            'zh-hant': '女性人名',
+            'zh-hk': '女性人名',
+            'zh-mo': '女性人名',
+            'zh-my': '女性人名',
+            'zh-sg': '女性人名',
+            'zh-tw': '女性人名'
+        }, 
+        'Q12308941': { #male given name
+            'af': 'manlike voornaam',
+            'ar': 'أسم مذكر معطى',
+            'ast': 'nome masculín',
+            'bar': 'Mannanam',
+            'be': 'мужчынскае асабістае імя',
+            'be-tarask': 'мужчынскае асабістае імя',
+            'bn': 'প্রদত্ত পুরুষ নাম',
+            'br': 'anv paotr',
+            'bs': 'muško ime',
+            'ca': 'prenom masculí',
+            'ce': 'стеган шен цӀе',
+            'cs': 'mužské křestní jméno',
+            'cy': 'enw personol gwrywaidd',
+            'da': 'drengenavn',
+            'de': 'männlicher Vorname',
+            'de-at': 'männlicher Vorname',
+            'de-ch': 'männlicher Vorname',
+            'el': 'ανδρικό όνομα',
+            'en': 'male given name',
+            'en-ca': 'male given name',
+            'en-gb': 'male given name',
+            'eo': 'vira persona nomo',
+            'es': 'nombre masculino',
+            'et': 'mehenimi',
+            'eu': 'gizonezko izena',
+            'fa': 'نام کوچک مردانه',
+            'fi': 'miehen etunimi',
+            'fr': 'prénom masculin',
+            'fy': 'Jongesnamme',
+            'gl': 'nome masculino',
+            'gsw': 'männlige Vorname',
+            'he': 'שם פרטי של גבר',
+            'hr': 'muško ime',
+            'hu': 'férfi keresztnév',
+            'hy': 'արական անձնանուն',
+            'id': 'nama pemberian maskulin',
+            'is': 'mannsnafn',
+            'it': 'prenome maschile',
+            'ja': '男性の名前',
+            'ko': '남성의 이름',
+            'la': 'praenomen masculinum',
+            'lb': 'männleche Virnumm',
+            'lt': 'vyriškas vardas',
+            'lv': 'vīriešu personvārds',
+            'mk': 'машко лично име',
+            'nb': 'mannsnavn',
+            'ne': 'पुलिङ्गी नाम',
+            'nl': 'mannelijke voornaam',
+            'nn': 'mannsnamn',
+            'pl': 'imię męskie',
+            'pt': 'nome próprio masculino',
+            'pt-br': 'nome próprio masculino',
+            'ro': 'prenume masculin',
+            'ru': 'мужское личное имя',
+            'scn': 'nomu di battìu masculinu',
+            'sco': 'male first name',
+            'sk': 'mužské meno',
+            'sl': 'moško osebno ime',
+            'sr': 'мушко лично име',
+            'sr-el': 'muško lično ime',
+            'sr-ec': 'мушко лично име',
+            'sv': 'mansnamn',
+            'tr': 'erkek ismidir',
+            'uk': 'чоловіче особове ім’я',
+            'yue': '男性人名',
+            'zh': '男性人名',
+            'zh-cn': '男性人名',
+            'zh-hans': '男性名',
+            'zh-hant': '男性人名',
+            'zh-hk': '男性人名',
+            'zh-mo': '男性人名',
+            'zh-my': '男性人名',
+            'zh-sg': '男性人名',
+            'zh-tw': '男性人名'
+        },         
+        'Q101352': { #surname
+            #'an': '', #no esta claro si es apellido o apelliu?
+            'ar': 'اسم العائلة', 
+            'ast': 'apellíu', 
+            'az': 'Soyad', 
+            'bar': 'Schreibnam', 
+            'be': 'прозвішча', 
+            'bg': 'презиме', 
+            'bn': 'পারিবারিক নাম', 
+            'bs': 'prezime', 
+            'ca': 'cognom', 
+            'cs': 'příjmení', 
+            'cy': 'cyfenw', 
+            'da': 'efternavn', 
+            'de': 'Familienname', 
+            'de-at': 'Familienname', 
+            'de-ch': 'Familienname', 
+            'el': 'επώνυμο', 
+            'en': 'family name', 
+            'es': 'apellido', 
+            'et': 'perekonnanimi', 
+            'eu': 'abizen', 
+            'fa': 'نام خانوادگی', 
+            'fi': 'sukunimi', 
+            'fo': 'ættarnavn',
+            'fr': 'nom de famille', 
+            'gl': 'apelido', 
+            'gsw': 'Familiename', 
+            'gu': 'અટક', 
+            'he': 'שם משפחה', 
+            'hr': 'prezime', 
+            'hu': 'vezetéknév', 
+            'hy': 'ազգանուն', 
+            'id': 'nama asli', 
+            'is': 'eftirnafn',
+            'it': 'cognome', 
+            'ja': '姓', 
+            'ka': 'გვარი', 
+            'ko': '성씨', 
+            'lb': 'Familljennumm', 
+            'lt': 'pavardė', 
+            'lv': 'uzvārds', 
+            'min': 'namo asli', 
+            'mk': 'презиме', 
+            'nb': 'etternavn', 
+            'nds': 'Familiennaam', 
+            'nl': 'achternaam', 
+            'nn': 'etternamn', 
+            'pl': 'nazwisko', 
+            'pt': 'sobrenome', 
+            'pt-br': 'nome de família', 
+            'ro': 'nume de familie', 
+            'ru': 'фамилия', 
+            'sh': 'prezime', 
+            'sk': 'priezvisko', 
+            'sl': 'priimek', 
+            'sq': 'mbiemri', 
+            'sr': 'презиме', 
+            'sv': 'efternamn', 
+            'tl': 'apelyido', 
+            'tr': 'soyadı', 
+            'uk': 'прізвище', 
+            'zh': '姓氏', 
+            'zh-cn': '姓氏', 
+            'zh-hans': '姓氏', 
+            'zh-hant': '姓氏', 
+            'zh-hk': '姓氏', 
+            'zh-mo': '姓氏', 
+            'zh-my': '姓氏', 
+            'zh-sg': '姓氏', 
+            'zh-tw': '姓氏',  
+            'zu': 'isibongo', 
+        }, 
+    }
+    site = pywikibot.Site('wikidata', 'wikidata')
+    repo = site.data_repository()
     
-    for result in json1['results']['bindings']:
-        q = 'item' in result and result['item']['value'].split('/entity/')[1] or ''
-        print('\n== %s ==' % (q))
+    for p31 in ['Q11879590', 'Q12308941', 'Q101352']:
+        url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=SELECT%20%3Fitem%0AWHERE%20%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3A'+p31+'%20.%0A%20%20%20%20FILTER%20NOT%20EXISTS%20%7B%20%3Fitem%20wdt%3AP31%20wd%3AQ4167410%20%7D%20.%20%0A%7D'
+        url = '%s&format=json' % (url)
+        sparql = getURL(url=url)
+        json1 = loadSPARQL(sparql=sparql)
         
-        item = pywikibot.ItemPage(repo, q)
-        item.get()
-        descriptions = item.descriptions
-        addedlangs = []
-        for lang in translations.keys():
-            if lang not in descriptions.keys():
-                descriptions[lang] = translations[lang]
-                addedlangs.append(lang)
-                #print('%s\tD%s\t"%s"' % (q, lang, translations[lang])) #quickstatements mode
-        data = { 'descriptions': descriptions }
-        addedlangs.sort()
-        if addedlangs:
-            summary = 'BOT - Adding descriptions (%s languages): %s' % (len(addedlangs), ', '.join(addedlangs))
-            print(summary)
-            try:
-                item.editEntity(data, summary=summary)
-            except:
-                print('Error while saving')
-                continue
+        for result in json1['results']['bindings']:
+            q = 'item' in result and result['item']['value'].split('/entity/')[1] or ''
+            print('\n== %s [%s] ==' % (q, p31))
+            item = pywikibot.ItemPage(repo, q)
+            item.get()
+            descriptions = item.descriptions
+            addedlangs = []
+            for lang in translations[p31].keys():
+                if lang not in descriptions.keys():
+                    descriptions[lang] = translations[p31][lang]
+                    addedlangs.append(lang)
+                    #print('%s\tD%s\t"%s"' % (q, lang, translations[p31][lang])) #quickstatements mode
+            data = { 'descriptions': descriptions }
+            addedlangs.sort()
+            if addedlangs:
+                summary = 'BOT - Adding descriptions (%s languages): %s' % (len(addedlangs), ', '.join(addedlangs))
+                print(summary)
+                try:
+                    item.editEntity(data, summary=summary)
+                except:
+                    print('Error while saving')
+                    continue
 
 if __name__ == "__main__":
     main()
