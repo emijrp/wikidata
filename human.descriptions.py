433daf2a46daa7c51798de1108bd5c5bd5bcc7b9
emijrp <emijrp@gmail.com>
2017-02-19 13:18:36 +0100

    counting errors

--- human.descriptions.py
+++ human.descriptions.py
@@ -51,6 +51,7 @@ def main():
     #ca: https://ca.wikipedia.org/wiki/Llista_de_gentilicis#Llista_de_gentilicis_per_estat
     #en: https://en.wikipedia.org/wiki/List_of_adjectival_and_demonymic_forms_for_countries_and_nations
     #es: https://es.wikipedia.org/wiki/Anexo:Gentilicios
+    #fr: https://fr.wikipedia.org/wiki/Liste_de_gentil%C3%A9s
     #gl: https://web.archive.org/web/20060512203621/http://www.galegoenlinna.uvigo.es/fichasVer.asp?idFicha=132
     translationsNationalities = {
         'Afghan': {
@@ -1913,11 +1914,13 @@ def main():
     c2 = 1
     total2 = 0
     cqueries = 0
+    errors = 0
     translations_list = list(translations.keys())
     translations_list.sort()
     totalqueries = len(translations_list) * len(targetlangs) * len(genders_list) # multiply by langs and genders
-    skiptolang = '' #'es'
-    skiptoperson = '' #'American politician'
+    skiptolang = 'es' #'es'
+    skiptogender = 'male' #'male'
+    skiptoperson = 'British athlete' #'American politician'
     for targetlang in targetlangs:
         if skiptolang:
             if skiptolang != targetlang:
@@ -1925,7 +1928,15 @@ def main():
                 continue
             else:
                 skiptolang = ''
+        
         for genderq, genderlabel in genders_list:
+            if skiptogender:
+                if skiptogender != genderlabel:
+                    print('Skiping gender:', genderlabel)
+                    continue
+                else:
+                    skiptogender = ''
+            
             for translation in translations_list:
                 print(targetlang, genderlabel, translation)
                 if skiptoperson:
@@ -1934,6 +1945,7 @@ def main():
                         continue
                     else:
                         skiptoperson = ''
+                
                 url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=SELECT%20%3Fitem%0AWHERE%20%7B%0A%20%20%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%20.%20%23instanceof%0A%20%20%20%20%3Fitem%20wdt%3AP21%20wd%3A'+genderq+'%20.%20%23gender%0A%20%20%20%20%3Fitem%20schema%3Adescription%20%22'+urllib.parse.quote(translation)+'%22%40en.%20%23description%0A%20%20%20%20OPTIONAL%20%7B%20%3Fitem%20schema%3Adescription%20%3FitemDescription.%20FILTER(LANG(%3FitemDescription)%20%3D%20%22'+targetlang+'%22).%20%20%7D%0A%20%20%20%20FILTER%20(!BOUND(%3FitemDescription))%0A%7D'
                 url = '%s&format=json' % (url)
                 sparql = getURL(url=url)
@@ -1944,13 +1956,14 @@ def main():
                 c = 1
                 for result in json1['results']['bindings']:
                     q = 'item' in result and result['item']['value'].split('/entity/')[1] or ''
-                    print('\n== %s (%d/%d; %s; %s; %s; items %d/%d; queries %d/%d) ==' % (q, c, total, translation, genderlabel, targetlang, c2, total2, cqueries, totalqueries))
+                    print('\n== %s (%d/%d; %s; %s; %s; items %d/%d; queries %d/%d; errors %d) ==' % (q, c, total, translation, genderlabel, targetlang, c2, total2, cqueries, totalqueries, errors))
                     c += 1
                     c2 += 1
                     item = pywikibot.ItemPage(repo, q)
                     try:
                         item.get()
                     except:
+                        errors += 1
                         print("Error while retrieving item")
                         continue
                     descriptions = item.descriptions
@@ -1967,6 +1980,7 @@ def main():
                         try:
                             item.editEntity(data, summary=summary)
                         except:
+                            errors += 1
                             print('Error while saving')
                             continue
                     else:
