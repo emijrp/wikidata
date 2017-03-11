959d43b399c69b66f40eacf3ec9fa50243b6fb3b
emijrp <emijrp@gmail.com>
2017-03-11 21:34:33 +0100

    bot stats

---
+++ bot.stats.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 emijrp <emijrp@gmail.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+
+import pwb
+import pywikibot
+
+def main():
+    site = pywikibot.Site('wikidata', 'wikidata')
+    stats = {}
+    
+    with open('bot.stats.txt') as f:
+        raw = f.read()
+    
+    for l in raw.splitlines():
+        x, y = l.strip().split('=')
+        x = x.strip().lower()
+        y = y.strip()
+        stats[x] = y and int(y) or 0
+    
+    output = """{| class="wikitable sortable" style="text-align: center;"
+|-
+| '''Edits''' || [[Special:Contributions/Emijrpbot|{{formatnum:%s}}]]
+|-
+| '''Labels''' || {{formatnum:%s}}
+|-
+| '''Descriptions''' || {{formatnum:%s}}
+|-
+| '''Aliases''' || {{formatnum:%s}}
+|-
+| colspan=2 | <small>Last update: %s</small>
+|}""" % (stats['edits'], stats['labels'], stats['descriptions'], stats['aliases'], datetime.datetime.now().strftime('%Y-%m-%d'))
+    
+    page = pywikibot.Page(site, 'User:Emijrpbot/stats')
+    page.text = output
+    page.save('BOT - Updating stats')
+
+if __name__ == "__main__":
+    main()
--- bot.stats.sh
+++ bot.stats.sh
@@ -1,14 +1,18 @@
-echo "SELECT rev_comment FROM revision WHERE rev_user_text='Emijrpbot';" > bot.stats.query
-sql wikidata < bot.stats.query > bot.stats.txt
+botnick="Emijrpbot"
 
-echo 'Edits'
-wc -l bot.stats.txt
+echo "SELECT rev_comment FROM revision WHERE rev_user_text='$botnick';" > bot.stats.query
+#sql wikidata < bot.stats.query > bot.stats.sql
 
-echo 'Labels'
-grep 'BOT - Adding labels' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
+edits=$(wc -l bot.stats.sql | cut -d" " -f1)
+echo "edits=$edits" | tee bot.stats.txt
 
-echo 'Descriptions'
-grep 'BOT - Adding descriptions' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
+labels=$(grep "BOT - Adding labels" bot.stats.sql | grep -ioE "[0-9]+ languages" | cut -d" " -f1 | awk "{ sum += \$1 } END { print sum }")
+echo "labels=$labels" | tee -a bot.stats.txt
 
-echo 'Aliases'
-grep 'BOT - Adding aliases' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
+descriptions=$(grep "BOT - Adding descriptions" bot.stats.sql | grep -ioE "[0-9]+ languages" | cut -d" " -f1 | awk "{ sum += \$1 } END { print sum }")
+echo "descriptions=$descriptions" | tee -a bot.stats.txt
+
+aliases=$(grep "BOT - Adding aliases" bot.stats.sql | grep -ioE "[0-9]+ languages" | cut -d" " -f1 | awk "{ sum += \$1 } END { print sum }")
+echo "aliases=$aliases" | tee -a bot.stats.txt
+
+python3 bot.stats.py
