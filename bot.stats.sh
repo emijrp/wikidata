2a8a9361bcff1266c177b77df7d7967dd735a2aa
emijrp <emijrp@gmail.com>
2017-03-11 15:27:17 +0100

    labels and aliases

--- bot.stats.sh
+++ bot.stats.sh
@@ -1,4 +1,11 @@
 echo "SELECT rev_comment FROM revision WHERE rev_user_text='Emijrpbot';" > bot.stats.query
 sql wikidata < bot.stats.query > bot.stats.txt
+
+echo 'Labels'
+grep 'BOT - Adding labels' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
+
 echo 'Descriptions'
 grep 'BOT - Adding descriptions' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
+
+echo 'Aliases'
+grep 'BOT - Adding aliases' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
