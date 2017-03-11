b7744d77d0b1a6d5932d0567dbd260b93c029343
emijrp <emijrp@gmail.com>
2017-03-11 15:39:36 +0100

    edits

--- bot.stats.sh
+++ bot.stats.sh
@@ -1,6 +1,9 @@
 echo "SELECT rev_comment FROM revision WHERE rev_user_text='Emijrpbot';" > bot.stats.query
 sql wikidata < bot.stats.query > bot.stats.txt
 
+echo 'Edits'
+wc -l bot.stats.txt
+
 echo 'Labels'
 grep 'BOT - Adding labels' bot.stats.txt | grep -ioE '[0-9]+ languages' | cut -d' ' -f1 | awk '{ sum += $1 } END { print sum }'
 
