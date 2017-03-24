18b401789ea0dac38818b2dadb24a1ca18bd20b0
emijrp <emijrp@gmail.com>
2017-03-24 20:06:14 +0100

    adding more stuff by country

--- human.aliases.es.py
+++ human.aliases.es.py
@@ -48,66 +48,73 @@ def main():
         'Q77',  #Uruguay
         'Q717', #Venezuela
     ]
-    skip = 'Q973088'
-    for country in countries:
-        url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=SELECT%20%3Fitem%0AWHERE%20%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3AQ5.%0A%20%20%20%20%3Fitem%20wdt%3AP27%20wd%3A'+country+'.%0A%7D'
-        url = '%s&format=json' % (url)
-        sparql = getURL(url=url)
-        json1 = loadSPARQL(sparql=sparql)
-        
-        for result in json1['results']['bindings']:
-            q = 'item' in result and result['item']['value'].split('/entity/')[1] or ''
-            print('==', q, '==')
-            if skip:
-                if skip != q:
-                    print('Skiping until', skip)
-                    continue
-                else:
-                    skip = ''
-            
-            item = pywikibot.ItemPage(repo, q)
-            try: #to detect Redirect because .isRedirectPage fails
-                item.get()
-            except:
-                print('Error while .get()')
-                continue
-            aliases = item.aliases
-            labels = item.labels
-            if not targetlang in labels:
-                print('Not label %s found' % (targetlang))
-                continue
-            
-            names = [labels[targetlang]]
-            if targetlang in aliases:
-                [names.append(x) for x in aliases[targetlang]]
-            
-            plainnames = []
-            for name in names:
-                if not re.search(r'[ñç\(\)\:]', name):
-                    #avoid producing names like Carlos I de España->Espana
-                    plainnames.append(removeAccents(name))
-            
-            missingnames = []
-            for plainname in plainnames:
-                if not plainname.lower() in [x.lower() for x in names] and \
-                   not plainname.lower() in [y.lower() for y in missingnames]:
-                    if targetlang in aliases.keys():
-                        aliases[targetlang].append(plainname)
-                    else:
-                        aliases[targetlang] = [plainname]
-                    missingnames.append(plainname)
-            
-            if missingnames:
-                data = { 'aliases': aliases }
-                missingnames.sort()
-                summary = "BOT - Adding %s aliases (%s): %s" % (len(missingnames), targetlang, ', '.join(missingnames))
-                print(summary.encode('utf-8'))
-                try:
-                    item.editEntity(data, summary=summary)
-                except:
-                    print('Error while saving')
-                    continue
     
+    skip = ''
+    for country in countries:
+        urls = [
+            'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query='+'SELECT%20%3Fitem%0AWHERE%20%7B%0A%09%3Fitem%20wdt%3AP17%20wd%3A'+country+'.%0A%7D', #stuff in these countries
+            'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query='+'SELECT%20%3Fitem%0AWHERE%20%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3AQ5.%0A%20%20%20%20%3Fitem%20wdt%3AP27%20wd%3A'+country+'.%0A%7D', #biographies by citizenship
+        ]
+        for url in urls:
+            url = '%s&format=json' % (url)
+            sparql = getURL(url=url)
+            json1 = loadSPARQL(sparql=sparql)
+            
+            for result in json1['results']['bindings']:
+                q = 'item' in result and result['item']['value'].split('/entity/')[1] or ''
+                if not q.startswith('Q'):
+                    continue
+                print('==', q, '==')
+                if skip:
+                    if skip != q:
+                        print('Skiping until', skip)
+                        continue
+                    else:
+                        skip = ''
+                
+                item = pywikibot.ItemPage(repo, q)
+                try: #to detect Redirect because .isRedirectPage fails
+                    item.get()
+                except:
+                    print('Error while .get()')
+                    continue
+                aliases = item.aliases
+                labels = item.labels
+                if not targetlang in labels:
+                    print('Not label %s found' % (targetlang))
+                    continue
+                
+                names = [labels[targetlang]]
+                if targetlang in aliases:
+                    [names.append(x) for x in aliases[targetlang]]
+                
+                plainnames = []
+                for name in names:
+                    if not re.search(r'[ñç\(\)\:]', name):
+                        #avoid producing names like Carlos I de España->Espana
+                        plainnames.append(removeAccents(name))
+                
+                missingnames = []
+                for plainname in plainnames:
+                    if not plainname.lower() in [x.lower() for x in names] and \
+                       not plainname.lower() in [y.lower() for y in missingnames]:
+                        if targetlang in aliases.keys():
+                            aliases[targetlang].append(plainname)
+                        else:
+                            aliases[targetlang] = [plainname]
+                        missingnames.append(plainname)
+                
+                if missingnames:
+                    data = { 'aliases': aliases }
+                    missingnames.sort()
+                    summary = "BOT - Adding %s aliases (%s): %s" % (len(missingnames), targetlang, ', '.join(missingnames))
+                    print(summary.encode('utf-8'))
+                    try:
+                        item.editEntity(data, summary=summary)
+                    except:
+                        print('Error while saving')
+                        continue
+        
     print("Finished successfully")
         
 if __name__ == "__main__":
