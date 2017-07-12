324d64219ecfc0c7903c2ec6881468a9e9eab8e7
emijrp <emijrp@gmail.com>
2017-07-12 21:26:16 +0200

    +en

--- all.aliases.es.py
+++ all.aliases.es.py
@@ -35,7 +35,7 @@ def main():
         'Q3409032', #unisex given name
         'Q82799', #name
     ]
-    targetlang = 'es'
+    targetlangs = ['en', 'es']
     site = pywikibot.Site('wikidata', 'wikidata')
     repo = site.data_repository()
     
@@ -55,65 +55,66 @@ def main():
             else:
                 skip = ''
         
-        item = pywikibot.ItemPage(repo, q)
-        try: #to detect Redirect because .isRedirectPage fails
-            item.get()
-        except:
-            print('Error while .get()')
-            continue
-        
-        excludeditem = False
-        if 'P31' in item.claims:
-            for p31 in item.claims['P31']:
-                try:
-                    #unknown value for .title() https://www.wikidata.org/wiki/Q22980688
-                    if p31.getTarget().title() in excludedP31:
-                        print('Skiping excluded item')
-                        excludeditem = True
-                        break
-                except:
-                    pass
-        if excludeditem:
-            continue
-        
-        aliases = item.aliases
-        labels = item.labels
-        if not targetlang in labels:
-            print('Not label %s found' % (targetlang))
-            continue
-        
-        names = [labels[targetlang]]
-        if targetlang in aliases:
-            [names.append(x) for x in aliases[targetlang]]
-        
-        plainnames = []
-        for name in names:
-            if not re.search(r'(?i)[^a-záéíóúàèìòùâêîôû\.\- ]', name):
-                #avoid producing names like Carlos I de España->Espana
-                plainnames.append(removeAccents(name))
-            else:
-                print('Filtering: %s' % (name.encode('utf-8')))
-        
-        missingnames = []
-        for plainname in plainnames:
-            if not plainname.lower() in [x.lower() for x in names] and \
-               not plainname.lower() in [y.lower() for y in missingnames]:
-                if targetlang in aliases.keys():
-                    aliases[targetlang].append(plainname)
-                else:
-                    aliases[targetlang] = [plainname]
-                missingnames.append(plainname)
-        
-        if missingnames:
-            data = { 'aliases': aliases }
-            missingnames.sort()
-            summary = "BOT - Adding %s aliases (%s): %s" % (len(missingnames), targetlang, ', '.join(missingnames))
-            print(summary.encode('utf-8'))
-            try:
-                item.editEntity(data, summary=summary)
+        for targetlang in targetlangs:
+            item = pywikibot.ItemPage(repo, q)
+            try: #to detect Redirect because .isRedirectPage fails
+                item.get()
             except:
-                print('Error while saving')
+                print('Error while .get()')
                 continue
+            
+            excludeditem = False
+            if 'P31' in item.claims:
+                for p31 in item.claims['P31']:
+                    try:
+                        #unknown value for .title() https://www.wikidata.org/wiki/Q22980688
+                        if p31.getTarget().title() in excludedP31:
+                            print('Skiping excluded item')
+                            excludeditem = True
+                            break
+                    except:
+                        pass
+            if excludeditem:
+                continue
+            
+            aliases = item.aliases
+            labels = item.labels
+            if not targetlang in labels:
+                print('Not label %s found' % (targetlang))
+                continue
+            
+            names = [labels[targetlang]]
+            if targetlang in aliases:
+                [names.append(x) for x in aliases[targetlang]]
+            
+            plainnames = []
+            for name in names:
+                if not re.search(r'(?i)[^a-záéíóúàèìòùâêîôû\.\- ]', name):
+                    #avoid producing names like Carlos I de España->Espana
+                    plainnames.append(removeAccents(name))
+                else:
+                    print('Filtering: %s' % (name.encode('utf-8')))
+            
+            missingnames = []
+            for plainname in plainnames:
+                if not plainname.lower() in [x.lower() for x in names] and \
+                   not plainname.lower() in [y.lower() for y in missingnames]:
+                    if targetlang in aliases.keys():
+                        aliases[targetlang].append(plainname)
+                    else:
+                        aliases[targetlang] = [plainname]
+                    missingnames.append(plainname)
+            
+            if missingnames:
+                data = { 'aliases': aliases }
+                missingnames.sort()
+                summary = "BOT - Adding %s aliases (%s): %s" % (len(missingnames), targetlang, ', '.join(missingnames))
+                print(summary.encode('utf-8'))
+                try:
+                    item.editEntity(data, summary=summary)
+                except:
+                    print('Error while saving')
+                    continue
     
     print("Finished successfully")
         
