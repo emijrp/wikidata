4766ebae2eb8adcbe19744e06c59aec29c5c5e1e
emijrp <emijrp@gmail.com>
2018-10-27 11:41:41 +0200

    .

--- all.human.knowledge.py
+++ all.human.knowledge.py
@@ -38,134 +38,139 @@ def getQueryCount(p='', q=''):
 
 def main():
     minsectionlevel = 2
-    #site = pywikibot.Site('en', 'wikipedia')
-    site = pywikibot.Site('wikidata', 'wikidata')
-    ahk = pywikibot.Page(site, 'User:Emijrp/All Human Knowledge')
-    ahktext = ahk.text
-    ahknewtext = ahktext
-    
-    #update inline stuff
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    #intro
-    wpenstatsurl = 'https://en.wikipedia.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=json'
-    jsonwpen = json.loads(getURL(url=wpenstatsurl))
-    wpenarticles = jsonwpen['query']['statistics']['articles']
-    wdstatsurl = 'https://www.wikidata.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=json'
-    jsonwd = json.loads(getURL(url=wdstatsurl))
-    wdarticles = jsonwd['query']['statistics']['articles']
-    wpenwdstats = "<!-- wpenwdstats -->As of {{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}, {{Q|Q328}} has {{formatnum:%s}} articles<ref>{{cite web | url=https://en.wikipedia.org/wiki/Special:Statistics | title=Special:Statistics | publisher=English Wikipedia | date=%s | accessdate=%s | quote=Content pages: {{formatnum:%s}}}}</ref> and {{Q|Q2013}} includes {{formatnum:%s}} items.<ref>{{cite web|url=https://www.wikidata.org/wiki/Special:Statistics | title=Special:Statistics | publisher=Wikidata | date=%s | accessdate=%s | quote=Content pages: {{formatnum:%s}}}}</ref><!-- /wpenwdstats -->" % (wpenarticles, today, today, wpenarticles, wdarticles, today, today, wdarticles)
-    ahknewtext = re.sub(r'<!-- wpenwdstats -->.*?<!-- /wpenwdstats -->', wpenwdstats, ahknewtext)
-    #biography
-    
-    #end update inline stuff
-    
-    #update tables
-    lines = ahknewtext.splitlines()
-    summarydic = {}
-    sections = []
-    newlines = []
-    newtotalwikidata = 0
-    newtotalestimate = 0
-    anchors = []
-    sectiontitle = ''
-    sectionlevel = 0
-    sectionparent = ''
-    anchor_r = r'(?i)(\{\{anchor\|([^\{\}]*?)\}\})'
-    row_r = r'({{User:Emijrp/AHKrow\|(P\d+)=([^\|\}]*?)\|wikidata=(\d*)\|estimate=(\d*))'
-    rowtotal_r = r'({{User:Emijrp/AHKrowtotal\|wikidata=(\d*)\|estimate=(\d*))'
-    for line in lines:
-        newline = line
+    enwpsite = pywikibot.Site('en', 'wikipedia')
+    wdsite = pywikibot.Site('wikidata', 'wikidata')
+    ahkpages = [
+        #[enwpsite, 'User:Emijrp/All Human Knowledge'], 
+        [wdsite, 'User:Emijrp/All Human Knowledge'], 
+    ]
+    for site, pagetitle in ahkpages:
+        ahk = pywikibot.Page(site, pagetitle)
+        ahktext = ahk.text
+        ahknewtext = ahktext
         
-        if newline.startswith('='*minsectionlevel) and newline.endswith('='*minsectionlevel):
-            sectionlevel = len(newline.split(' ')[0].strip())
-            sectiontitle = newline.replace('=', '').strip()
+        #update inline stuff
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        #intro
+        wpenstatsurl = 'https://en.wikipedia.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=json'
+        jsonwpen = json.loads(getURL(url=wpenstatsurl))
+        wpenarticles = jsonwpen['query']['statistics']['articles']
+        wdstatsurl = 'https://www.wikidata.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=json'
+        jsonwd = json.loads(getURL(url=wdstatsurl))
+        wdarticles = jsonwd['query']['statistics']['articles']
+        wpenwdstats = "<!-- wpenwdstats -->As of {{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}, {{Q|Q328}} has {{formatnum:%s}} articles<ref>{{cite web | url=https://en.wikipedia.org/wiki/Special:Statistics | title=Special:Statistics | publisher=English Wikipedia | date=%s | accessdate=%s | quote=Content pages: {{formatnum:%s}}}}</ref> and {{Q|Q2013}} includes {{formatnum:%s}} items.<ref>{{cite web|url=https://www.wikidata.org/wiki/Special:Statistics | title=Special:Statistics | publisher=Wikidata | date=%s | accessdate=%s | quote=Content pages: {{formatnum:%s}}}}</ref><!-- /wpenwdstats -->" % (wpenarticles, today, today, wpenarticles, wdarticles, today, today, wdarticles)
+        ahknewtext = re.sub(r'<!-- wpenwdstats -->.*?<!-- /wpenwdstats -->', wpenwdstats, ahknewtext)
+        #biography
+        
+        #end update inline stuff
+        
+        #update tables
+        lines = ahknewtext.splitlines()
+        summarydic = {}
+        sections = []
+        newlines = []
+        newtotalwikidata = 0
+        newtotalestimate = 0
+        anchors = []
+        sectiontitle = ''
+        sectionlevel = 0
+        sectionparent = ''
+        anchor_r = r'(?i)(\{\{anchor\|([^\{\}]*?)\}\})'
+        row_r = r'({{User:Emijrp/AHKrow\|(P\d+)=([^\|\}]*?)\|wikidata=(\d*)\|estimate=(\d*))'
+        rowtotal_r = r'({{User:Emijrp/AHKrowtotal\|wikidata=(\d*)\|estimate=(\d*))'
+        for line in lines:
+            newline = line
+            
+            if newline.startswith('='*minsectionlevel) and newline.endswith('='*minsectionlevel):
+                sectionlevel = len(newline.split(' ')[0].strip())
+                sectiontitle = newline.replace('=', '').strip()
+                if sectionlevel == minsectionlevel:
+                    sectionparent = sectiontitle
+                    sections.append([sectiontitle, sectionlevel])
+            
+            #anchors
+            if re.search(anchor_r, newline):
+                m = re.findall(anchor_r, newline)
+                for i in m:
+                    x, y = i
+                    for anchor in y.split('|'):
+                        anchor = anchor.strip()
+                        if anchor and not anchor in anchors:
+                            anchors.append(anchor)
+            
+            #update row
+            if re.search(row_r, newline):
+                m = re.findall(row_r, newline)
+                for i in m:
+                    row, p, q, wikidata, estimate = i
+                    newwikidata = getQueryCount(p=p, q=q)
+                    newrow = row.replace('wikidata=%s' % (wikidata), 'wikidata=%s' % (newwikidata))
+                    newline = newline.replace(row, newrow)
+                    if not 'exclude=yes' in newline: #don't use newrow as row_r doesn't parse this param
+                        newtotalwikidata += newwikidata and int(newwikidata) or 0
+                        newtotalestimate += estimate and int(estimate) or (newwikidata and int(newwikidata) or 0)
+            
+            #update row total
+            m = re.findall(rowtotal_r, newline)
+            for i in m:
+                totalrow, totalwikidata, totalestimate = i
+                newtotalrow = totalrow
+                newtotalrow = newtotalrow.replace('wikidata=%s' % (totalwikidata), 'wikidata=%s' % (newtotalwikidata))
+                newtotalrow = newtotalrow.replace('estimate=%s' % (totalestimate), 'estimate=%s' % (newtotalestimate))
+                newline = newline.replace(totalrow, newtotalrow)
+                if sectionlevel > minsectionlevel:
+                    sections.append([sectiontitle, sectionlevel])
+                summarydic[sectiontitle] = { 'parent': sectionparent, 'wikidata': newtotalwikidata, 'estimate': newtotalestimate, 'anchors': anchors }
+                #reset
+                newtotalwikidata = 0
+                newtotalestimate = 0
+                anchors = []
+            
+            pywikibot.showDiff(line, newline)
+            newlines.append(newline)
+        ahknewtext = '\n'.join(newlines)
+        
+        #update summary
+        summaryrows = []
+        summarytotalwikidata = 0
+        summarytotalestimate = 0
+        for sectiontitle, sectionlevel in sections:
+            summaryrow = ''
             if sectionlevel == minsectionlevel:
-                sectionparent = sectiontitle
-                sections.append([sectiontitle, sectionlevel])
-        
-        #anchors
-        if re.search(anchor_r, newline):
-            m = re.findall(anchor_r, newline)
-            for i in m:
-                x, y = i
-                for anchor in y.split('|'):
-                    anchor = anchor.strip()
-                    if anchor and not anchor in anchors:
-                        anchors.append(anchor)
-        
-        #update row
-        if re.search(row_r, newline):
-            m = re.findall(row_r, newline)
-            for i in m:
-                row, p, q, wikidata, estimate = i
-                newwikidata = getQueryCount(p=p, q=q)
-                newrow = row.replace('wikidata=%s' % (wikidata), 'wikidata=%s' % (newwikidata))
-                newline = newline.replace(row, newrow)
-                if not 'exclude=yes' in newline: #don't use newrow as row_r doesn't parse this param
-                    newtotalwikidata += newwikidata and int(newwikidata) or 0
-                    newtotalestimate += estimate and int(estimate) or (newwikidata and int(newwikidata) or 0)
-        
-        #update row total
-        m = re.findall(rowtotal_r, newline)
-        for i in m:
-            totalrow, totalwikidata, totalestimate = i
-            newtotalrow = totalrow
-            newtotalrow = newtotalrow.replace('wikidata=%s' % (totalwikidata), 'wikidata=%s' % (newtotalwikidata))
-            newtotalrow = newtotalrow.replace('estimate=%s' % (totalestimate), 'estimate=%s' % (newtotalestimate))
-            newline = newline.replace(totalrow, newtotalrow)
-            if sectionlevel > minsectionlevel:
-                sections.append([sectiontitle, sectionlevel])
-            summarydic[sectiontitle] = { 'parent': sectionparent, 'wikidata': newtotalwikidata, 'estimate': newtotalestimate, 'anchors': anchors }
-            #reset
-            newtotalwikidata = 0
-            newtotalestimate = 0
-            anchors = []
-        
-        pywikibot.showDiff(line, newline)
-        newlines.append(newline)
-    ahknewtext = '\n'.join(newlines)
-    
-    #update summary
-    summaryrows = []
-    summarytotalwikidata = 0
-    summarytotalestimate = 0
-    for sectiontitle, sectionlevel in sections:
-        summaryrow = ''
-        if sectionlevel == minsectionlevel:
-            rowspan = 0
-            for x, y in summarydic.items():
-                if y['parent'] == sectiontitle:
-                    rowspan += 1
-            if rowspan == 1:
-                anchors = '{{·}} '.join(['[[#%s|%s]]' % (anchor, anchor) for anchor in summarydic[sectiontitle]['anchors']])
-                if not anchors:
-                    anchors = '[[#%s|See table]]' % (sectiontitle)
-                summaryrow = """| [[#%s|%s]]
+                rowspan = 0
+                for x, y in summarydic.items():
+                    if y['parent'] == sectiontitle:
+                        rowspan += 1
+                if rowspan == 1:
+                    anchors = '{{·}} '.join(['[[#%s|%s]]' % (anchor, anchor) for anchor in summarydic[sectiontitle]['anchors']])
+                    if not anchors:
+                        anchors = '[[#%s|See table]]' % (sectiontitle)
+                    summaryrow = """| [[#%s|%s]]
 | <li>[[#%s|%s]]
 {{User:Emijrp/AHKsummaryrow|wikidata=%s|estimate=%s}}
 | %s
 |-""" % (sectiontitle, sectiontitle, sectiontitle, sectiontitle, summarydic[sectiontitle]['wikidata'], summarydic[sectiontitle]['estimate'], anchors)
-                summarytotalwikidata += summarydic[sectiontitle]['wikidata']
-                summarytotalestimate += summarydic[sectiontitle]['estimate']
-            elif rowspan > 1:
-                summaryrow = """| rowspan=%s | [[#%s|%s]]
+                    summarytotalwikidata += summarydic[sectiontitle]['wikidata']
+                    summarytotalestimate += summarydic[sectiontitle]['estimate']
+                elif rowspan > 1:
+                    summaryrow = """| rowspan=%s | [[#%s|%s]]
 |-""" % (rowspan+1, sectiontitle, sectiontitle)
-        elif sectionlevel > minsectionlevel:
-            anchors = '{{·}} '.join(['[[#%s|%s]]' % (anchor, anchor) for anchor in summarydic[sectiontitle]['anchors']])
-            if not anchors:
-                anchors = '[[#%s|See table]]' % (sectiontitle)
-            summaryrow = """| <li>[[#%s|%s]]
+            elif sectionlevel > minsectionlevel:
+                anchors = '{{·}} '.join(['[[#%s|%s]]' % (anchor, anchor) for anchor in summarydic[sectiontitle]['anchors']])
+                if not anchors:
+                    anchors = '[[#%s|See table]]' % (sectiontitle)
+                summaryrow = """| <li>[[#%s|%s]]
 {{User:Emijrp/AHKsummaryrow|wikidata=%s|estimate=%s}}
 | %s
 |-""" % (sectiontitle, sectiontitle, summarydic[sectiontitle]['wikidata'], summarydic[sectiontitle]['estimate'], anchors)
-            summarytotalwikidata += summarydic[sectiontitle]['wikidata']
-            summarytotalestimate += summarydic[sectiontitle]['estimate']
-        else:
-            continue
-        if summaryrow:
-            summaryrows.append(summaryrow)
-    summarytotal = "{{User:Emijrp/AHKsummarytotal|wikidata=%s|estimate=%s}}" % (summarytotalwikidata, summarytotalestimate)
-    summary = """<!-- summary -->{| class="wikitable sortable plainlinks"
+                summarytotalwikidata += summarydic[sectiontitle]['wikidata']
+                summarytotalestimate += summarydic[sectiontitle]['estimate']
+            else:
+                continue
+            if summaryrow:
+                summaryrows.append(summaryrow)
+        summarytotal = "{{User:Emijrp/AHKsummarytotal|wikidata=%s|estimate=%s}}" % (summarytotalwikidata, summarytotalestimate)
+        summary = """<!-- summary -->{| class="wikitable sortable plainlinks"
 ! width="100px" | Topic
 ! width="150px" | Subtopic
 ! Wikidata
@@ -175,15 +180,15 @@ def main():
 %s
 %s
 |}<!-- /summary -->""" % ('\n'.join(summaryrows), summarytotal)
-    ahknewtext = '%s%s%s' % (ahknewtext.split('<!-- summary -->')[0], summary, ahknewtext.split('<!-- /summary -->')[1])
-    
-    #ahk inline
-    ahknewtext = re.sub(r'<!-- ahk -->.*?<!-- /ahk -->', '<!-- ahk -->{{formatnum:%s}}<!-- /ahk -->' % (summarytotalestimate), ahknewtext)
-    
-    if ahknewtext and ahktext != ahknewtext:
-        pywikibot.showDiff(ahktext, ahknewtext)
-        ahk.text = ahknewtext
-        ahk.save('BOT - Updating The Catalogue of Catalogues')
+        ahknewtext = '%s%s%s' % (ahknewtext.split('<!-- summary -->')[0], summary, ahknewtext.split('<!-- /summary -->')[1])
+        
+        #ahk inline
+        ahknewtext = re.sub(r'<!-- ahk -->.*?<!-- /ahk -->', '<!-- ahk -->{{formatnum:%s}}<!-- /ahk -->' % (summarytotalestimate), ahknewtext)
+        
+        if ahknewtext and ahktext != ahknewtext:
+            pywikibot.showDiff(ahktext, ahknewtext)
+            ahk.text = ahknewtext
+            ahk.save('BOT - Updating The Catalogue of Catalogues')
     
 if __name__ == '__main__':
     main()
