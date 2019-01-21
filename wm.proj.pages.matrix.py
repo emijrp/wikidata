60844411cb387e4308909e4f5788421841ff2992
emijrp <emijrp@gmail.com>
2019-01-21 22:00:24 +0100

    .

--- wm.proj.pages.matrix.py
+++ wm.proj.pages.matrix.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2017 emijrp <emijrp@gmail.com>
+# Copyright (C) 2017-2018 emijrp <emijrp@gmail.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
