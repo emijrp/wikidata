f2a2291bfafdf477490ad99953ae098c8b39fd32
emijrp <emijrp@gmail.com>
2022-05-27 00:09:35 +0200

    kubernetes script

---
+++ pwbvenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 -m venv pwbvenv
+source pwbvenv/bin/activate
+pip3 install --upgrade pip "setuptools>=49.4.0, !=50.0.0, <50.2.0" wheel
+pip3 install pywikibot
+pip3 install mwparserfromhell
+
+#toolforge-jobs run pwbvenv --command "./pwbvenv.sh" --image tf-python39 --wait
+#toolforge-jobs run simbad-test1 --command "pwbvenv/bin/python simbad.py" --image tf-python39
+#toolforge-jobs run taxon-test1 --command "pwbvenv/bin/python taxon.py" --image tf-python39
