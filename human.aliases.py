5f8d0605024b5d0c50b4f4e23f715f066a2ce514
emijrp <emijrp@gmail.com>
2024-02-22 20:58:28 +0100

    .

--- human.aliases.py
+++ human.aliases.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2021-2022 emijrp <emijrp@gmail.com>
+# Copyright (C) 2021-2024 emijrp <emijrp@gmail.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
