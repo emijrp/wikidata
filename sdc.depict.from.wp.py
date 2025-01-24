04d4fec35f42211a7f0548f702dc17f4b10d9094
user <emijrp@gmail.com>
2025-01-24 17:37:43 +0100

    .

--- sdc.depict.from.wp.py
+++ sdc.depict.from.wp.py
@@ -52,7 +52,7 @@ def main():
 			if page.isRedirectPage():
 				continue
 			c += 1
-			if c >= 5000:
+			if c >= 25000:
 				c = 0
 				print("###Skiping to other Wikipedia region")
 				break #this is to randomize more
